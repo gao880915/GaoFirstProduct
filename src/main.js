c699e1a3913aa1288773dba15f22e33a306473e3
unknown <gaoxiangfeng123@qq.com>
2018-05-07 17:58:48 +0800

    headerArrow

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import './assets/style/reset.css'
 import 'swiper/dist/css/swiper.css'
+import './assets/style/iconfont/iconfont.css'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
